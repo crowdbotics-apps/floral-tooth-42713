8b4da3df13625107123fbeee4b28788fad4f702c
Crowdbotics <team@crowdbotics.com>
2023-08-07 23:26:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-tooth-42713",
+  url: "https://floral-tooth-42713.botics.co",
+  api: "https://floral-tooth-42713.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
